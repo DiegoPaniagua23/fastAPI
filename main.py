3f24aa583251cd797751ef43be9b9faddf0fabf9
Diego Paniagua <diegopaniaguamolina@gmail.com>
2025-07-25 22:50:28 -0600

    Creating seccions with diff urls

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI 
 
 app = FastAPI()
- 
+
+# Request Get method url: "/"
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Welcome to my FastAPI!"}
+  
+@app.get("/posts")
+def get_posts():
+    return {"data": "These are your posts!"}
