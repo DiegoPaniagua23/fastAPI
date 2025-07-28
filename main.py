58f14e2286b366fdbb2148ccc2985b4b5aac4c5b
Diego Paniagua <diegopaniaguamolina@gmail.com>
2025-07-27 18:01:41 -0600

    HTTP post reques method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI 
+from fastapi import Body, FastAPI 
 
 app = FastAPI()
 
@@ -10,3 +10,8 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "These are your posts!"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']}, content: {payload['content']}"}
